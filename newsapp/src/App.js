4385e9781bed0c10f2e537f4e95e6c4d7c715391
chirag-aa <chiragambwani259@gmail.com>
2021-07-04 15:38:49 +0530

    ok

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
 import Tempapp from "./components/Tempapp";
 import Newsapp from './components/Newsapp';
-//const{Meta}=Card;
-function App() {
-  return (
-   <Newsapp />
+export default function App(){
+
+  const ReturnOnlyLI = () => {
+    return(
+     <>
+      <Tempapp />
+      </>
+    )
+  }
+
+  return(
+    <div>
+         <ReturnOnlyLI />
+        <Newsapp />
+       
+    </div>
   );
 }
-
-export default App;
--- newsapp/src/components/Tempapp.js
+++ newsapp/src/components/Tempapp.js
@@ -1,12 +1,33 @@
-// import logo from './logo.svg';
-// import React,{useState,useEffect} from "react";
-// import axios from "axios";
+
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import "./tempstyle.css";
 
 const Tempapp = ()=>{
+    const [city,setcity]=useState();
+    const [search,setsearch]=useState("Enter City");
+
+    useEffect(()=>{
+         const fetchApi= async()=>{
+             const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=cbe0136f9161eb5d645e0f7f777a6c11`
+             const response =await axios.get(url);
+            //  const resjson=await response.json();
+             console.log(response);
+         }
+
+         fetchApi();
+    } ,[setsearch])
       return (
           <>
+          <h1> THE WALL NEWS</h1>
           <div className="temp">
-              <input type="search" className="inpsearch" ></input>
+              <input type="search" className="inpsearch" onChange={(event)=>{
+                     setsearch(event.target.value);
+              }} ></input>
+          </div>
+
+          <div className="information">
+                <h1>{search}</h1>
           </div>
           </>
       )
