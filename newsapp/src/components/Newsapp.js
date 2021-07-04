b75946c8cc9eb41f711a6e5984bc0b86863d6ec3
chirag-aa <chiragambwani259@gmail.com>
2021-07-04 14:02:19 +0530

    ok

---
+++ newsapp/src/components/Newsapp.js
@@ -0,0 +1,38 @@
+
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import "./style.css";
+
+function Newsapp() {
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+      const loadNews = async () => {
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=4c250882d56f4d90bf9887e909a7a510`
+        )
+        setNews(response.data.articles);
+      };
+      loadNews();
+    }, []);
+    console.log(news);
+    return (
+      <div className="App">
+        { 
+           news.map((currentElement)=>{
+            return( 
+                <div className="first">
+                <p className="TitleClass">{currentElement.title}</p>
+                <img src={currentElement.urlToImage} alt="ham image hai" className="imgClass"></img>
+                <p className="descripClass">{currentElement.description}</p>
+                <button className="readmoreClass">
+                  <a href={currentElement.url} target="_blank">Read More</a>
+                </button>
+                </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+  
+  export default Newsapp;
---
+++ newsapp/src/components/Tempapp.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import React,{useState,useEffect} from "react";
+// import axios from "axios";
+
+const Tempapp = ()=>{
+      return (
+          <>
+          <div className="temp">
+              <input type="search" className="inpsearch" ></input>
+          </div>
+          </>
+      )
+}
+export default Tempapp;
