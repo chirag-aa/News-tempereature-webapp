dfe0e98d749c3d2b9f53cc5164e3881d3757159f
chirag-aa <chiragambwani259@gmail.com>
2021-07-12 09:26:38 +0530

    ok

--- newsapp/src/components/Tempapp.js
+++ newsapp/src/components/Tempapp.js
@@ -1,43 +1,44 @@
 
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./tempstyle.css";
 
-const Tempapp = ()=>{
-    const [city,setcity]=useState();
-    const [search,setsearch]=useState("delhi");
+const Tempapp = () => {
+  const [city, setcity] = useState();
+  const [search, setsearch] = useState("delhi");
 
-    useEffect(()=>{
-         const fetchApi= async()=>{
-             const url=`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=cbe0136f9161eb5d645e0f7f777a6c11`
-             const response =await fetch(url);
-             const resjson=await response.json();
-            console.log(resjson.main);
-             setcity(resjson.main);
-           // console.log(resjson);
-         };
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=cbe0136f9161eb5d645e0f7f777a6c11`
+      const response = await fetch(url);
+      const resjson = await response.json();
+      console.log(resjson.main);
+      setcity(resjson.main);
+      // console.log(resjson);
+    };
 
-         fetchApi();
-    } ,[search])
-      return (
-          <>
-          <h1> Temperature App</h1>
-          <div className="temp">
-              <input type="search" className="inpsearch" onChange={(event)=>{
-                     setsearch(event.target.value);
-              }} ></input>
-          </div>
-        {!city ? (
-             <p>No Data Found</p>
-        ) :(
-            <div className="information">
-                <h1>City: {search}</h1>
-                <h1>Temp: {city.temp}</h1> 
-                <h1>Max Temp: {city.temp_max}</h1> 
-                <h1>Min Teamp: {city.temp_min}</h1> 
-          </div> 
-        )}
-          </>
-      )
+    fetchApi();
+  }, [search])
+  return (
+    <>
+      <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/indian-express-logo-n.svg" title="The Indian Express" alt="The Indian Express" class="-logo"></img>
+      <h1> Temperature App</h1>
+      <div className="temp">
+        <input type="search" className="inpsearch" onChange={(event) => {
+          setsearch(event.target.value);
+        }} ></input>
+      </div>
+      {!city ? (
+        <p>No Data Found</p>
+      ) : (
+        <div className="information">
+          <h1>City: {search}</h1>
+          <h1>Temp: {city.temp}</h1>
+          <h1>Max Temp: {city.temp_max}</h1>
+          <h1>Min Teamp: {city.temp_min}</h1>
+        </div>
+      )}
+    </>
+  )
 }
 export default Tempapp;
